970f2860d74e9dba9e92034939f8c6745d0f201d
chezwhite <contact@asblanco.com>
2017-04-18 13:14:57 -0700

    database_url

--- squizer_server/settings.py
+++ squizer_server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,7 +79,7 @@ WSGI_APPLICATION = 'squizer_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config(default=os.environ["HEROKU_POSTGRESQL_COPPER_URL"])}
+DATABASES = {'default': dj_database_url.config(default=os.environ["DATABASE_URL"])}
 
 """
 Local database
