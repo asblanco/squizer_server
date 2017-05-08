2779be6158490c4133191222ebd840a57671d013
chezwhite <contact@asblanco.com>
2017-05-07 19:15:38 -0700

    Heroku static

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from courses import views
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -19,3 +20,8 @@ urlpatterns = [
     url(r'^retrieve-test/(?P<pk>[0-9]+)/$', views.RetrieveTest.as_view()),
     url(r'^', include(router.urls))
 ]
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
