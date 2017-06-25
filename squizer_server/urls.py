2d6fe6a4bd38d2f53bde08e7a6062804b71df6f9
chezwhite <contact@asblanco.com>
2017-06-25 20:51:48 +0200

    rest_framework_docs

--- squizer_server/urls.py
+++ squizer_server/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('squizer.urls')),
+    url(r'^docs/', include('rest_framework_docs.urls')),
 ]
